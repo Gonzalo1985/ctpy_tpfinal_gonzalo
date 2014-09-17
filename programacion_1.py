5031bd3ad2795e61beb617e765e9945684d26cd7
Gonzalo1985 <gonzalomartindiaz@hotmail.com>
2014-09-16 22:38:54 -0300

    El ejercicio 1 de la sección Programación...

--- programacion_1.py
+++ programacion_1.py
@@ -6,17 +6,13 @@
 # Define y guarda la función n factorial
 def n_factorial(number):
   assert number>=0,"El numero no puede ser menor a cero para calcular factorial"
-  n_factorial = number
-  for i in range(1,number):
-    n_factorial = n_factorial*(number-i)
+  if number==0:
+    n_factorial=1
+  else:
+    n_factorial = number
+    for i in range(1,number):
+      n_factorial = n_factorial*(number-i)
   return n_factorial
-
-# Define y guarda la función inversa de n factorial
-def n_factorial_inv(number):
-  from decimal import Decimal
-  assert number>0,"El numero no puede ser menor ni igual a cero para calcular función inversa de n factorial"
-  n_factorial_inv = Decimal(1)/Decimal(n_factorial(number))
-  return n_factorial_inv
 
 # Define y guarda la función sumatoria de i hasta n
 def sum_to_n(number):
@@ -26,6 +22,13 @@ def sum_to_n(number):
     sum_to_n = sum_to_n + i
   return sum_to_n
 
+# Define y guarda la función sumatoria de 1/i hasta n
+def sum_to_inv_n(number):
+  assert number>0,"El numero no puede ser menor ni igual a cero para calcular la sumatoria de 1/i hasta n"
+  sum_to_inv_n = 0
+  for i in range(1,number+1):
+    sum_to_inv_n = sum_to_inv_n + 1.0/i
+  return sum_to_inv_n
 
 ############################################################
 
@@ -36,10 +39,10 @@ def main():
   parser.add_argument("Number", type=int, help="Debe ser un valor entero")
   args = parser.parse_args()
 
-  print "La sumatoria hasta n es: "+str(sum_to_n(args.Number))
   print "El factorial del número elegido es: "+str(n_factorial(args.Number))
-  print "La inversa del factorial del número elegido es: "+str(n_factorial_inv(args.Number))
-
+  print "La inversa del factorial del número elegido es: "+str(1.0/n_factorial(args.Number))
+  print "La sumatoria de i hasta n es: "+str(sum_to_n(args.Number))
+  print "La sumatoria de 1/i hasta n es: "+str(sum_to_inv_n(args.Number))
 
 if __name__ == "__main__":
   main()
